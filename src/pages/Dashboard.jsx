bbdbcb036cc85483ae4305948ad9cbd765fe487a
Chinmoy Podder <chinmoypodder22@gmail.com>
2025-05-09 03:35:08 +0600

    gadget ecommerce website

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,52 +3,72 @@ import Heading from "../components/Heading";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
 import { SlEqualizer } from "react-icons/sl";
-import { getStoredProductList, getStoredWishList, removeStoredProduct, removeStoredWishProduct } from "../utility/addRemoveProduct";
+import {
+  getStoredProductList,
+  getStoredWishList,
+  removeStoredProduct,
+  removeStoredWishProduct,
+} from "../utility/addRemoveProduct";
 import CartWish from "../components/CartWish";
 import ModalImg from "../assets/Group.png";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import WishList from "../components/WishLIst";
 
-
-const Dashboard = ({price}) => {
-  document.title="Dashboard | Gadget Heaven";
-  console.log(price);
+const Dashboard = () => {
+  document.title = "Dashboard | Gadget Heaven";
 
   const navigate = useNavigate();
   const [addCartList, setAddCartList] = useState([]);
+  console.log(addCartList);
   const [addWishList, setAddWishList] = useState([]);
 
-  useEffect(()=>{
+  const [total, setTotal] = useState(0);
+
+  useEffect(() => {
+    let sum = 0;
+    addCartList.map(product => {
+      sum += product.price;
+    });
+    setTotal(sum);
+  }, [addCartList]);
+
+
+
+  useEffect(() => {
     const storedAddProductList = getStoredProductList();
     setAddCartList(storedAddProductList);
-  },[])
+  }, []);
 
-  useEffect(() =>{
+  useEffect(() => {
     const storedWishProductList = getStoredWishList();
     setAddWishList(storedWishProductList);
-  },[])
+  }, []);
 
-  const handleSortBy = (sortItem) => {
-    if(sortItem){
-      const sortedItems = [...addCartList].sort((a, b)=>b.price - a.price);
+  const handleSortBy = () => {
+    // if (sortItem) {
+    //   const sortedItems = [...addCartList].sort((a, b) => b.price - a.price);
+    //   setAddCartList(sortedItems);
+    // }
+    const sortedItems = [...addCartList].sort((a, b) => b.price - a.price);
       setAddCartList(sortedItems);
-    }
-  }
+  };
 
-  const handleRemoveProduct = (id)=>{
+  const handleRemoveProduct = (id) => {
     removeStoredProduct(id);
     const addCartProduct = getStoredProductList();
     setAddCartList(addCartProduct);
+  };
 
-    
+  const handlePurchase = () =>{
+    document.getElementById("my_modal_4").showModal()
   }
 
-  const handleWishListRemoveProduct = (id) =>{
+  const handleWishListRemoveProduct = (id) => {
     removeStoredWishProduct(id);
     const addWishProduct = getStoredWishList();
     setAddWishList(addWishProduct);
-  }
+  };
 
   return (
     <>
@@ -80,50 +100,77 @@ const Dashboard = ({price}) => {
             <TabPanel>
               <div className="flex flex-col md:flex-row items-center md:justify-between">
                 <div>
-                <h2 className="text-2xl font-bold">Cart: {addCartList.length}</h2>
+                  <h2 className="text-2xl font-bold">
+                    Cart: {addCartList.length}
+                  </h2>
                 </div>
                 <div className="flex flex-col md:flex-row items-center gap-5">
-                  <p className="text-2xl font-bold">Total cost: 0</p>
-                  <button onClick={() => handleSortBy("price")} className="btn flex items-center shadow-none rounded-full text-[#9538E2] gap-2 text-lg font-semibold border-2 border-[#9538E2] bg-transparent hover:bg-transparent">Sort By Price <SlEqualizer /></button>
+                  <p className="text-2xl font-bold">Total cost: {total}</p>
+                  <button
+                    onClick={() => handleSortBy()}
+                    className="btn flex items-center shadow-none rounded-full text-[#9538E2] gap-2 text-lg font-semibold border-2 border-[#9538E2] bg-transparent hover:bg-transparent"
+                  >
+                    Sort By Price <SlEqualizer />
+                  </button>
 
                   {/* You can open the modal using document.getElementById('ID').showModal() method */}
-                    <button className="btn text-lg font-semibold  shadow-none rounded-full text-white border bg-gradient-to-b from-[#9538E2] to-fuchsia-500" onClick={()=>document.getElementById('my_modal_4').showModal()}>Purchase</button>
-                    <dialog id="my_modal_4" className="modal">
-                      <div className="modal-box flex flex-col items-center max-w-xs">
-                        <div>
-                          <img src={ModalImg} alt="" />
-                        </div>
-                        <h2 className="text-2xl font-bold pt-2">Payment Successful</h2>
-                        <div className="divider"></div>
-                        <p className="text-gray-500">Thanks for purchasing</p>
-                        <p className="py-2 text-gray-500">Total: 0</p>
+                  <button
+                    className="btn text-lg font-semibold  shadow-none rounded-full text-white border bg-gradient-to-b from-[#9538E2] to-fuchsia-500"
+                    onClick={() => handlePurchase()}
+                  >
+                    Purchase
+                  </button>
+                  <dialog id="my_modal_4" className="modal">
+                    <div className="modal-box flex flex-col items-center max-w-xs">
+                      <div>
+                        <img src={ModalImg} alt="" />
+                      </div>
+                      <h2 className="text-2xl font-bold pt-2">
+                        Payment Successful
+                      </h2>
+                      <div className="divider"></div>
+                      <p className="text-gray-500">Thanks for purchasing</p>
+                      <p className="py-2 text-gray-500">Total: {total}</p>
 
-                        <div className="w-full mx-auto">
-                          <form method="dialog">
-                            {/* if there is a button, it will close the modal */}
-                            <button onClick={() => {
+                      <div className="w-full mx-auto">
+                        <form method="dialog">
+                          {/* if there is a button, it will close the modal */}
+                          <button
+                            onClick={() => {
                               navigate("/");
                               toast.success("Back To Home", {
                                 position: "top-center",
                                 autoClose: 800,
                                 theme: "colored",
-                                });
-                            }} className="btn w-full rounded-full">Close</button>
-                          </form>
-                        </div>
+                              });
+                            }}
+                            className="btn w-full rounded-full"
+                          >
+                            Close
+                          </button>
+                        </form>
                       </div>
-                    </dialog>
+                    </div>
+                  </dialog>
                 </div>
               </div>
-              {
-                addCartList.map((product) => <CartWish key={product.id} product={product} handleRemoveProduct={handleRemoveProduct}></CartWish>)
-              }
+              {addCartList.map((product) => (
+                <CartWish
+                  key={product.id}
+                  product={product}
+                  handleRemoveProduct={handleRemoveProduct}
+                ></CartWish>
+              ))}
             </TabPanel>
             <TabPanel>
               <h2 className="text-2xl font-bold">Wish Read: </h2>
-              {
-                addWishList.map((product)=> <WishList key={product.id} product={product} handleWishListRemoveProduct={handleWishListRemoveProduct}></WishList>)
-              }
+              {addWishList.map((product) => (
+                <WishList
+                  key={product.id}
+                  product={product}
+                  handleWishListRemoveProduct={handleWishListRemoveProduct}
+                ></WishList>
+              ))}
             </TabPanel>
           </div>
         </Tabs>
